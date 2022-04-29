0a8df234ceb800b23e2ff6dcde873a2a9756a776
alouwyck <andy.louwyck@gmail.com>
2022-04-29 20:52:48 +0200

    module heuristic.py: add class BS for beam search

--- SearchExerciser/search/heuristic.py
+++ SearchExerciser/search/heuristic.py
@@ -37,3 +37,35 @@ class GS(HeuristicSearch):
         super()._add_new_paths_to_queue()
         self._queue = sorted(self._queue, key=lambda path: self.heuristic(path.last_state()))
 
+
+class BS(HeuristicSearch):
+    # class that implements beam search
+
+    name = "Beam search"
+
+    def __init__(self, initial_queue, heuristic, width, print_result=True, print_queue=False):
+        super().__init__(initial_queue, heuristic, print_result, print_queue)
+        self.width = width
+
+    def _remove_path_from_queue(self):
+        pass  # new paths are created to all children of all paths in the queue...
+
+    def _create_new_paths(self):
+        # creates new paths to all children of all paths in the queue
+        # rejects the new paths with loops
+        self._new_paths = self._queue_class([new_path for path in self._queue for new_path in path.calculate_children()
+                                             if not new_path.has_loop()])  # optimization: and not new_path.is_leaf()?
+
+    def _add_new_paths_to_queue(self):
+        # sorts the new paths by heuristic f
+        # adds the width best new paths to the queue
+        self._new_paths = sorted(self._new_paths, key=lambda path: self.heuristic(path.last_state()))
+        self._queue = self._queue_class(self._new_paths[:self.width])
+
+    def _check_goal_is_reached(self):
+        # updates goal_is_reached
+        for path in self._queue:
+            if path.reaches_goal():
+                self.goal_is_reached = True
+                self.path_to_goal = path
+                break
