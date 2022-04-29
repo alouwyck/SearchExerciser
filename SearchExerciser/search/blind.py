0a06da45f96ce557bcc055d47e137c0ade7bd58a
alouwyck <andy.louwyck@gmail.com>
2022-04-29 18:56:29 +0200

    refactor blind.py

--- SearchExerciser/search/blind.py
+++ SearchExerciser/search/blind.py
@@ -1,148 +1,7 @@
-from abc import ABC, abstractmethod
-from copy import deepcopy
+from .base import Base, Algorithm
 from time import time
 import numpy as np
 from random import randint
-
-
-class Algorithm(ABC):
-    # abstract superclass for implementing search algorithms
-
-    name = "search algorithm"  # name of algorithm (string)
-
-    def __init__(self, initial_queue, print_result=True, print_queue=False, heuristic=None, depth_limit=np.Inf):
-        # initial_queue is PathSeries object
-        # print_result is boolean, default is True
-        # print_queue is boolean, default is False
-        # heuristic is a function, default is None
-        # depth_limit limits search to given depth, default is Inf, meaning there is no limit
-        self.initial_queue = initial_queue
-        self.print_result = print_result
-        self.print_queue = print_queue
-        self.heuristic = heuristic
-        self.depth_limit = depth_limit
-        self._initialize()
-
-    def _initialize(self):
-        # initialise attributes
-        # called by constructor and by method search
-        self.path_to_goal = None
-        self.goal_is_reached = False
-        self._queue = deepcopy(self.initial_queue)
-        self._queue_class = type(self.initial_queue)
-        self.length_queue = [len(self._queue)]
-        self.elapsed_time = None
-        self.nr_iterations = 0
-        self._first_path = None
-        self._new_paths = None
-
-    def search(self):
-        # performs the implemented search algorithm
-
-        # start time
-        starttime = time()
-
-        # initialize
-        self._initialize()
-
-        # print initial queue
-        self._print_initial_queue()
-
-        # 2. while (queue is not empty and goal is not reached)
-        self.goal_is_reached = self._queue[0].reaches_goal()
-        if self.goal_is_reached:
-            self.path_to_goal = self._queue[0]
-        while self._queue and not self.goal_is_reached:
-
-            # augment number of iterations
-            self.nr_iterations += 1
-
-            # length queue
-            self.length_queue.append(len(self._queue))
-
-            # remove the first path from the queue
-            self._first_path = self._queue.pop(0)
-
-            # check depth_limit
-            if len(self._first_path) < self.depth_limit:
-
-                # create new paths (to all children)
-                # reject the new paths with loops
-                self._new_paths = self._queue_class([path for path in self._first_path.calculate_children()
-                                                     if not path.has_loop()])
-
-                # add the new paths to the queue
-                self._add_new_paths_to_queue()
-
-                # print queue
-                self._print_queue()
-
-                # update goal_is_reached
-                for path in self._new_paths:
-                    if path.reaches_goal():
-                        self.goal_is_reached = True
-                        self.path_to_goal = path
-                        break
-
-        # elapsed time
-        self.elapsed_time = time() - starttime
-
-        # print path to goal
-        self._print_path_to_goal()
-
-        # print result
-        self._print_result()
-
-    @abstractmethod
-    def _add_new_paths_to_queue(self):
-        # adds new child paths to queue
-        pass
-
-    def _print_result(self):
-        # prints result
-        # if self.print_result is True
-        if self.print_result:
-            # 3. if (goal reached) then success else failure
-            print("ALGORITHM:", self.name)
-            print("RESULT:", "SUCCES" if self.goal_is_reached else "FAILURE")
-            # print elapsed time, number of iterations, and maximum length of queue
-            print("Elapsed time:", self.elapsed_time, 'seconds')
-            print('Number of iterations:', self.nr_iterations)
-            print('Maximum length of queue:', max(self.length_queue))
-
-    def _print_initial_queue(self):
-        # prints initial queue
-        # if self.print_queue is True
-        if self.print_queue:
-            print("Initial queue:")
-            print(self._queue)
-            print()
-
-    def _print_queue(self):
-        # prints queue
-        # if self.print_queue is True
-        if self.print_queue:
-            print(f"Iteration {self.nr_iterations}")
-            print("Path removed from queue:")
-            print(self._first_path)
-            print("New paths:")
-            print(self._new_paths)
-            print("Paths in queue:")
-            print(self._queue)
-            print()
-
-    def _print_path_to_goal(self):
-        # prints path to goal
-        # if self.print_result is True and path to goal if found
-        if self.print_queue and self.path_to_goal is not None:
-            print("Path to goal found in new paths:")
-            print(self.path_to_goal)
-            print()
-
-    def __repr__(self):
-        # overrides inherited __repr__ method
-        # returns string
-        return self.name
 
 
 class DFS(Algorithm):
@@ -150,12 +9,27 @@ class DFS(Algorithm):
 
     name = "Depth-first search"
 
-    def __init__(self, initial_queue, print_result=True, print_queue=False, heuristic=None, depth_limit=np.Inf):
-        super().__init__(initial_queue, print_result, print_queue, heuristic, depth_limit)
+    def __init__(self, initial_queue, print_result=True, print_queue=False, depth_limit=np.Inf):
+        super().__init__(initial_queue, print_result, print_queue)
+        self.depth_limit = depth_limit
+
+    def _create_new_paths(self):
+        # creates new children if length of first path is smaller than depth limit
+        if len(self._first_path) < self.depth_limit:  # check depth_limit
+            super()._create_new_paths()
 
     def _add_new_paths_to_queue(self):
         # adds the new paths to the FRONT of the queue
-        self._queue = self._queue_class(self._new_paths) + self._queue
+        if len(self._first_path) < self.depth_limit:  # check depth_limit
+            self._queue = self._queue_class(self._new_paths) + self._queue
+
+    def _check_goal_is_reached(self):
+        if len(self._first_path) < self.depth_limit:  # check depth_limit
+            super()._check_goal_is_reached()
+
+    def _print_queue(self):
+        if len(self._first_path) < self.depth_limit:  # check depth_limit
+            super()._print_queue()
 
 
 class BFS(Algorithm):
@@ -163,8 +37,8 @@ class BFS(Algorithm):
 
     name = "Breadth-first search"
 
-    def __init__(self, initial_queue, print_result=True, print_queue=False, heuristic=None):
-        super().__init__(initial_queue, print_result, print_queue, heuristic)
+    def __init__(self, initial_queue, print_result=True, print_queue=False):
+        super().__init__(initial_queue, print_result, print_queue)
 
     def _add_new_paths_to_queue(self):
         # adds the new paths to the BACK of the queue
@@ -176,8 +50,8 @@ class NDS(Algorithm):
 
     name = "Non-deterministic search"
 
-    def __init__(self, initial_queue, print_result=True, print_queue=False, heuristic=None):
-        super().__init__(initial_queue, print_result, print_queue, heuristic)
+    def __init__(self, initial_queue, print_result=True, print_queue=False):
+        super().__init__(initial_queue, print_result, print_queue)
 
     def _add_new_paths_to_queue(self):
         # adds the new paths in RANDOM places in the queue
@@ -187,15 +61,16 @@ class NDS(Algorithm):
             self._queue.insert(index, path)
 
 
-class IDS(Algorithm):
+class IDS(Base):
     # class that implements iterative deepening search
 
     name = "Iterative deepening search"
 
-    def __init__(self, initial_queue, print_result=True, print_queue=False, heuristic=None):
-        super().__init__(initial_queue, print_result, print_queue, heuristic)
+    def __init__(self, initial_queue, print_result=True, print_queue=False):
+        super().__init__(initial_queue, print_result, print_queue)
         # create DFS object for depth-limited search
-        self.__depth_limited_dfs = DFS(initial_queue, print_result, print_queue, heuristic)
+        self.__depth_limited_dfs = DFS(initial_queue, print_result, print_queue)
+        self.depth_limit = None
 
     def search(self):
 
@@ -211,7 +86,7 @@ class IDS(Algorithm):
         # 2. WHILE goal is not reached
         self.goal_is_reached = self.initial_queue[0].reaches_goal()
         if self.goal_is_reached:
-            self.path_to_goal = self._queue[0]
+            self.path_to_goal = self.initial_queue[0]
 
         while not self.goal_is_reached:
 
@@ -233,7 +108,7 @@ class IDS(Algorithm):
             self.nr_iterations += self.__depth_limited_dfs.nr_iterations
 
             # length queue
-            self.length_queue += self.__depth_limited_dfs.length_queue
+            self.queue_lengths += self.__depth_limited_dfs.queue_lengths
 
             # DEPTH = DEPTH + 1
             self.depth_limit += 1
@@ -248,6 +123,3 @@ class IDS(Algorithm):
         if self.print_result:
             print("--> FINAL RESULT")
             super()._print_result()
-
-    def _add_new_paths_to_queue(self):
-        pass
