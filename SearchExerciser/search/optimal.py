a05acb3891943f25fcab15458f42dc93099ed366
alouwyck <andy.louwyck@gmail.com>
2022-04-30 21:06:56 +0200

    printing h or f when displaying queue if heuristic or optimal search (update)

--- SearchExerciser/search/optimal.py
+++ SearchExerciser/search/optimal.py
@@ -52,6 +52,7 @@ class BBUC(OUC):
     def __init__(self, initial_queue, print_result=True, print_queue=False):
         super().__init__(initial_queue, print_result, print_queue)
         self._bound = np.Inf
+        self._pruned = None
 
     def _initialize(self):
         # initialize attributes
@@ -68,7 +69,25 @@ class BBUC(OUC):
         for path in self._new_paths:
             if path.reaches_goal():
                 self._bound = min(path.cost, self._bound)
+        if self.print_queue:
+            self._pruned = self._queue_class(filter(lambda path: path.cost > self._bound, self._queue))
         self._queue = self._queue_class(filter(lambda path: path.cost <= self._bound, self._queue))
+
+    def _print_queue(self):
+        # prints queue
+        # if self.print_queue is True
+        if self.print_queue:
+            print(f"Iteration {self.nr_iterations}")
+            print("Path removed from queue:")
+            print(self._first_path.string_to_print(**self._print_options))
+            print("New paths:")
+            print(self._new_paths.string_to_print(**self._print_options))
+            if self._pruned:
+                print("Pruned paths:")
+                print(self._pruned.string_to_print(**self._print_options))
+            print("Paths in queue:")
+            print(self._queue.string_to_print(**self._print_options))
+            print()
 
 
 class EEUC(OUC):
@@ -94,11 +113,29 @@ class AS(EEUC):
 
     def __init__(self, initial_queue, print_result=True, print_queue=False):
         super().__init__(initial_queue, print_result, print_queue)
+        self._redundant = None
 
     def _add_new_paths_to_queue(self):
         # adds the new paths to the front of the queue
         # sorts the entire queue by f-value (= accumulated cost + heuristic h)
         # deletes redundant paths
         super()._add_new_paths_to_queue()
-        to_remove = [p for p, q in permutations(self._queue, 2) if q.contains_state(p[-1]) and p.cost >= q.cost]
-        self._queue = self._queue_class([path for path in self._queue if path not in to_remove])
+        self._redundant = self._queue_class([p for p, q in permutations(self._queue, 2)
+                                             if q.contains_state(p[-1]) and p.cost >= q.cost])
+        self._queue = self._queue_class([path for path in self._queue if path not in self._redundant])
+
+    def _print_queue(self):
+        # prints queue
+        # if self.print_queue is True
+        if self.print_queue:
+            print(f"Iteration {self.nr_iterations}")
+            print("Path removed from queue:")
+            print(self._first_path.string_to_print(**self._print_options))
+            print("New paths:")
+            print(self._new_paths.string_to_print(**self._print_options))
+            if self._redundant:
+                print("Redundant paths:")
+                print(self._redundant.string_to_print(**self._print_options))
+            print("Paths in queue:")
+            print(self._queue.string_to_print(**self._print_options))
+            print()
